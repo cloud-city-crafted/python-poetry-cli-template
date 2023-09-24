1f4102894bb3159b61dbf53451b48a5e2209c1de
Lando Toothman <25360446+TheLandolorien@users.noreply.github.com>
2023-09-24 01:40:10 -0400

    feat: Add --version flag

--- tests/e2e/test_cli.py
+++ tests/e2e/test_cli.py
@@ -1,15 +1,26 @@
 import os
+import re
+
+SEMVAR_PATTERN = (
+    r"^(?P<major>0|[1-9]\d*)\."
+    r"(?P<minor>0|[1-9]\d*)\."
+    r"(?P<patch>0|[1-9]\d*)"
+    r"(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)"
+    r"(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?"
+    r"(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+)
 
 
 def test_cli_default() -> None:
     output = os.popen("poetry run package-name").read()
 
     assert output == (
-        "usage: package-name [-h] [--zen]\n\n"
+        "usage: package-name [-h] [--version] [--zen]\n\n"
         "Package description\n\n"
         "options:\n"
-        "  -h, --help  show this help message and exit\n"
-        "  --zen, -z   print a random sentence of Zen\n"
+        "  -h, --help     show this help message and exit\n"
+        "  --version, -v  print version\n"
+        "  --zen, -z      print a random sentence of Zen\n"
     ), "should display default output"
 
 
@@ -18,3 +29,10 @@ def test_cli_zen_flag() -> None:
 
     assert output.endswith(".\n"), "should return zen sentence"
     assert "usage" not in output, "should not print usage"
+
+
+def test_cli_version_flag() -> None:
+    output = os.popen("poetry run package-name --version").read()
+
+    match = re.match(SEMVAR_PATTERN, output.strip())
+    assert match is not None, "should print a semvar-compliant version"
--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -5,19 +5,35 @@ from package_name import cli
 
 @patch("package_name.cli.requests")
 @patch("package_name.cli.argparse._sys.argv")
-def test_cli_default_command(mock_argv, mock_requests) -> None:
+def test_cli_default(mock_argv, mock_requests, capsys) -> None:
     mock_argv.__getitem__.return_value = []
 
     cli.run()
 
+    assert capsys.readouterr().out.startswith("usage: package-name"), "should Zen sentence"
     mock_requests.get.assert_not_called()
 
 
 @patch("package_name.cli.requests")
 @patch("package_name.cli.argparse._sys.argv")
-def test_cli_zen_command(mock_argv, mock_requests) -> None:
+def test_cli_zen(mock_argv, mock_requests, capsys) -> None:
+    mock_requests.get.return_value.text = "Foo."
     mock_argv.__getitem__.return_value = ["--zen"]
 
     cli.run()
 
+    assert capsys.readouterr().out == "Foo.\n", "should Zen sentence"
     mock_requests.get.assert_called_once_with(url="https://api.github.com/zen", timeout=30)
+
+
+@patch("package_name.cli.metadata")
+@patch("package_name.cli.requests")
+@patch("package_name.cli.argparse._sys.argv")
+def test_cli_version(mock_argv, mock_requests, mock_metadata, capsys) -> None:
+    mock_argv.__getitem__.return_value = ["--version"]
+    mock_metadata.version.return_value = "1.1.0a0"
+
+    cli.run()
+
+    assert capsys.readouterr().out == "1.1.0a0\n", "should print version"
+    mock_requests.get.assert_not_called()
